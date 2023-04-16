5aa1a0c08f256a1d0baca12cef0cb40f5f992009
Oleksandr Tereshchenko <toporok@gmail.com>
2023-04-16 23:15:51 +0300

    swipy_client

--- swipy_client/openai_patch.py
+++ swipy_client/openai_patch.py
@@ -1,4 +1,6 @@
 """Patch openai to log requests to Swipy Platform."""
+from typing import Any
+
 import openai
 
 from swipy_client.swipy_requestor import log_text_completion_request
@@ -8,8 +10,8 @@ def patch_openai() -> None:
     """Patch openai to log requests to Swipy Platform."""
     chat_completion_create_original = openai.ChatCompletion.create
 
-    def swipy_chat_completion_create(*args, **kwargs) -> dict:
-        log_text_completion_request()
+    def swipy_chat_completion_create(*args, **kwargs) -> dict[str, Any]:
+        log_text_completion_request("openai.ChatCompletion.create", args, kwargs)
         return chat_completion_create_original(*args, **kwargs)
 
     openai.ChatCompletion.create = swipy_chat_completion_create
--- swipy_client/swipy_requestor.py
+++ swipy_client/swipy_requestor.py
@@ -1,12 +1,21 @@
 """This module is responsible for sending requests to Swipy Platform."""
+from typing import Any
+
 import httpx
 
 _client = httpx.Client()
 
 
-def log_text_completion_request() -> None:
+def log_text_completion_request(
+    caller_name: str, args: tuple[Any, ...], kwargs: dict[str, Any]
+) -> None:
     """Log a text completion request to Swipy Platform."""
+    payload = {
+        "caller_name": caller_name,
+        "args": args,
+        "kwargs": kwargs,
+    }
     _client.post(
         "http://localhost:8000/swipy_client_webhook/",
-        json={"hello": "world"},
+        json=payload,
     )
